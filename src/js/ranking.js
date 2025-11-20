250113faae94550593c0418b9f8c5960c1babc0a
Henrique Santos Silva <henriqueesquecemuito@gmail.com>
2025-11-20 13:40:50 -0300

    Sort Ranking

--- src/js/ranking.js
+++ src/js/ranking.js
@@ -3,9 +3,10 @@ import { jogadores } from "./data.js";
 const divRanking = document.querySelector(".ranking");
 
 divRanking.innerHTML += jogadores
+  .sort((a, b) => b.pontos - a.pontos)
   .map((j, i) => {
     return `
-    
+
       <div class="player">
           <span class="pos">${i + 1}</span>
           <span class="name">${j.nome}</span>
