0a186dfc3fa04c5d11aea204c27fc0a9167abddf
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-11-24 15:37:46 -0300

    Up

--- src/js/data.js
+++ src/js/data.js
@@ -3,18 +3,18 @@ const dadosLocalStorage = localStorage.getItem("jogadores");
 export const jogadores = dadosLocalStorage
   ? JSON.parse(dadosLocalStorage)
   : [
-      {
-        nome: "Jogador Exemplo",
-        nivel: "pleno",
-        pontos: 200,
-        missoes: [
-          {
-            nome: "Ajudar o aldeão",
-            dificuldade: "Fácil",
-            tempo: "10 min",
-            pontos: 100,
-            status: "pendente",
-          },
-        ],
-      },
-    ];
+    // {
+    //   nome: "Jogador Exemplo",
+    //   nivel: "pleno",
+    //   pontos: 200,
+    //   missoes: [
+    //     {
+    //       nome: "Ajudar o aldeão",
+    //       dificuldade: "Fácil",
+    //       tempo: "10 min",
+    //       pontos: 100,
+    //       status: "pendente",
+    //     },
+    //   ],
+    // },
+  ];
