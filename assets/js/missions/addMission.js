541a3c43c7c7fd045a5fba302f2251245fe2cfd0
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-12-01 19:44:37 -0300

    Update addMission.js

--- assets/js/missions/addMission.js
+++ assets/js/missions/addMission.js
@@ -72,7 +72,7 @@ export function initAddMission(
     );
 
     setTimeout(() => {
-      window.location.href = "../index.html";
+      window.location.href = "/missions-project/index.html";
     }, timeOut);
   });
 }
